0b6b9a2762701659c4e01ed47d9385b0114b9473
Ratovonirina <eric.ratovonirina@gmail.com>
2024-02-27 08:09:04 +0300

    Material Parcours

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatIconModule } from  '@angular/material/icon' ;
 import { MatDividerModule } from  '@angular/material/divider' ;
 import { MatButtonModule } from  '@angular/material/button' ;
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule , MatIconModule],
+  imports: [MatGridListModule, MatButtonModule, MatDividerModule , MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
