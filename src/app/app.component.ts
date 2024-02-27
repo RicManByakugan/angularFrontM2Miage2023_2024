741b17c3f262d8be9fab3795a1c156456af91df5
Ratovonirina <eric.ratovonirina@gmail.com>
2024-02-27 07:22:57 +0300

    Train button with material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from "@angular/material/button" 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatSlideToggleModule],
+  imports: [RouterOutlet, HomeComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
